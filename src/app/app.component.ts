eeb61c7f57c46f5400f4171303afc819da8423a4
Jacob Lopez <jacobtl2342@gmail.com>
2018-05-12 13:20:50 -0500

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,10 @@
-import {Component } from '@angular/core';
-import { MouseEvent } from '@agm/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-
 export class AppComponent {
-  title: string = 'AptSquad';
-  lat: number = 41.881832;
-  lng: number = -87.623177;
+  title = 'app';
 }
