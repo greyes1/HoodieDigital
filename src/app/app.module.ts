2657b2029b72078854ee7b9ffa18cbf31462f903
Jacob Lopez <jacobtl2342@gmail.com>
2018-05-16 12:53:06 -0500

    Material design
    Button now appears correctly

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, ApplicationRef } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AppComponent } from './app.component';
-import {FormsModule} from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 import { AgmCoreModule } from '@agm/core';
 import { MatButtonModule, MatCheckboxModule } from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material.module';
 
 @NgModule({
   declarations: [
@@ -16,13 +18,14 @@ import { MatButtonModule, MatCheckboxModule } from '@angular/material';
     FormsModule,
     MatButtonModule,
     MatCheckboxModule,
+    BrowserAnimationsModule,
+    MaterialModule,
     AgmCoreModule.forRoot({
       // please get your own API key here:AIzaSyDzAW9YMqqCV-x7U7Oy-McPz5BtFcU5fsg
       // https://developers.google.com/maps/documentation/javascript/get-api-key?hl=en
       apiKey: 'AIzaSyDzAW9YMqqCV-x7U7Oy-McPz5BtFcU5fsg'
     })
   ],
-  exports: [MatButtonModule, MatCheckboxModule],
   providers: [],
   bootstrap: [AppComponent]
 })
