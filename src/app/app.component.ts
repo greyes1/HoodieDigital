f763eb546a2eab215c8615ce8b0653391e7da9b0
thatgreyes1 <thatgreyes@gmail.com>
2018-05-22 16:41:16 -0500

    Added angular google maps-based search bar, and set the stage for http pulling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
-import {Component } from '@angular/core';
+import { Component, OnInit, NgZone, ViewChild, ElementRef } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { MapsAPILoader } from '@agm/core';
+
 
 @Component({
   selector: 'app-root',
@@ -6,25 +9,61 @@ import {Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 
-export class AppComponent {
+export class AppComponent implements OnInit{
+
   title: string = 'AptSquad';
-  lat: number = 41.8781;
-  lng: number = -87.6298;
+  public lat: number = 41.8781;
+  public lng: number = -87.6298;
+  public zoom: number = 8;
+  public searchControl: FormControl;
+
+  @ViewChild("search")
+  public searchElementRef: ElementRef;
 
   markers: marker[] = [
     {
-      markerLat : 41.8827,
-      markerLng : -87.6233,
-      label : "Bean",
-      draggable : false
+      markerLat: 41.8827,
+      markerLng: -87.6233,
+      label: "Bean",
+      draggable: false
     },
     {
-      markerLat : 41.8789,
-      markerLng : -87.6359,
-      label : "Willis Tower",
-      draggable : false
+      markerLat: 41.8789,
+      markerLng: -87.6359,
+      label: "Willis Tower",
+      draggable: false
     }
   ];
+
+  constructor(
+    private mapsAPILoader: MapsAPILoader,
+    private ngZone: NgZone
+  ) {}
+
+  ngOnInit() {
+    this.mapsAPILoader.load().then(() => {
+      let autocomplete = new google.maps.places.Autocomplete(this.searchElementRef.nativeElement, {
+        types: ["address"]
+      });
+      autocomplete.addListener("place_changed", () => {
+        this.ngZone.run(() => {
+          //get the place result
+          let place: google.maps.places.PlaceResult = autocomplete.getPlace();
+
+          //verify result
+          if (place.geometry === undefined || place.geometry === null) {
+            return;
+          }
+
+          //set latitude, longitude and zoom
+          this.lat = place.geometry.location.lat();
+          this.lng = place.geometry.location.lng();
+          this.zoom = 12;
+        });
+      });
+    });
+
+  }
 }
 interface marker{
   markerLat: number;
