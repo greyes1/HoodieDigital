732c4fe9b21acdbd5d29c6d0f4a3039f4db0e9ad
thatgreyes1 <thatgreyes@gmail.com>
2018-05-27 17:21:39 -0500

    Added angular google maps-based search bar, and set the stage for http pulling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,102 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, NgZone, ViewChild, ElementRef } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { MapsAPILoader } from '@agm/core';
+import { } from 'googlemaps';
+
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+
+export class AppComponent implements OnInit{
+
+  title: string = 'AptSquad';
+  public lat: number = 41.8781;
+  public lng: number = -87.6298;
+  public zoom: number = 8;
+  public searchControl: FormControl;
+
+  @ViewChild("search")
+  public searchElementRef: ElementRef;
+
+  public markers: marker[] = [
+    {
+      markerLat: 41.8827,
+      markerLng: -87.6233,
+      label: "Bean",
+      draggable: false
+    },
+    {
+      markerLat: 41.8789,
+      markerLng: -87.6359,
+      label: "Willis Tower",
+      draggable: false
+    }
+  ];
+  navLinks: link[] = [
+    {
+      label: "College"
+    },
+    {
+      label: "Restaurants"
+    },
+    {
+      label: "Lifestyle"
+    },
+    {
+      label: "Crime"
+    },
+    {
+      label: "Entertainment"
+    },
+    {
+      label: "Transit"
+    }
+  ];
+
+  constructor(
+    private mapsAPILoader: MapsAPILoader,
+    private ngZone: NgZone
+  ) {}
+
+  ngOnInit() {
+    this.mapsAPILoader.load().then(() => {
+      let autocomplete = new google.maps.places.Autocomplete(this.searchElementRef.nativeElement, {
+        types: ["address"]
+      });
+      autocomplete.addListener("place_changed", () => {
+        this.ngZone.run(() => {
+          //get the place result
+          let place: google.maps.places.PlaceResult = autocomplete.getPlace();
+
+          //verify result
+          if (place.geometry === undefined || place.geometry === null) {
+            return;
+          }
+          //This is where the data is added to the array that displays to the map
+          this.markers.push({markerLat: place.geometry.location.lat(), markerLng: place.geometry.location.lng(),
+                            label: place.name, draggable: false});
+
+          //set latitude, longitude and zoom
+          this.lat = place.geometry.location.lat();
+          this.lng = place.geometry.location.lng();
+          this.zoom = 15;
+        });
+      });
+    });
+
+  }
 }
+interface marker{
+  markerLat: number;
+  markerLng: number;
+  label?: string;
+  draggable: boolean;
+}
+interface link{
+  label?: string;
+}
+
